50dbceb22b601730608980eadb084deaad15f905
Sidronio Lima <sidronio@hotmail.com>
2025-01-30 16:37:15 -0300

    Code first commit

---
+++ src/main/java/br/com/sidroniolima/xpe/configuration/WebServerConfing.java
@@ -0,0 +1,9 @@
+package br.com.sidroniolima.xpe.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("br.com.sidroniolima.xpe")
+public class WebServerConfing {
+}
